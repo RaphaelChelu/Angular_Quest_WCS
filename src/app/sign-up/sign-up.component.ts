b1347d5cc8dc0765b6686fcb7a2a1efdd784bf3e
Raphael <raphaelchelu@gmail.com>
2018-10-03 13:42:35 +0200

    Quete Angular Component

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  email = Email;
+  
   constructor() { }
 
   ngOnInit() {
