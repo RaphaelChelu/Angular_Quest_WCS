6ccd015c1d33d404b48bca4af23ef39633688b91
RaphaelChelu <raphaelchelu@gmail.com>
2018-11-07 15:48:42 +0100

    Quete 03 - Angular - Le Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
@@ -13,7 +14,14 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        { path: "sign-up", component: SignUpComponent, pathMatch: "full"},
+        { path: "user-profile", component: UserProfileComponent, pathMatch: "full" }, 
+      ], 
+      {useHash: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
